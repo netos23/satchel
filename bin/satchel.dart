67ebdf997184299369b367601e6b3028e093dd58
netos23 <nikmoroz2002@outlook.com>
2025-03-06 19:42:35 +0300

    init commit

---
+++ bin/satchel.dart
@@ -0,0 +1,5 @@
+import 'package:satchel/satchel.dart' as satchel;
+
+void main(List<String> arguments) {
+  print('Hello world: ${satchel.calculate()}!');
+}
---
+++ test/satchel_test.dart
@@ -0,0 +1,8 @@
+import 'package:satchel/satchel.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
