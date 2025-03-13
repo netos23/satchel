c7ede3d07e9b3f777b082d9311f9f233fb5d9fe6
netos23 <nikmoroz2002@outlook.com>
2025-03-13 18:14:33 +0300

    feature(types): add types mapping

--- lib/src/type_checker/stella_types.dart
+++ lib/src/type_checker/stella_types.dart
@@ -2,6 +2,14 @@ import 'package:collection/collection.dart';
 
 sealed class StellaType {
   const StellaType();
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is StellaType && runtimeType == other.runtimeType;
+
+  @override
+  int get hashCode => 0;
 }
 
 class Bool extends StellaType {
@@ -18,6 +26,17 @@ class TypeRef extends StellaType {
   const TypeRef({
     required this.type,
   });
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      super == other &&
+          other is TypeRef &&
+          runtimeType == other.runtimeType &&
+          type == other.type;
+
+  @override
+  int get hashCode => super.hashCode ^ type.hashCode;
 }
 
 class TypeSum extends StellaType {
@@ -28,47 +47,19 @@ class TypeSum extends StellaType {
     required this.left,
     required this.right,
   });
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      super == other &&
+          other is TypeSum &&
+          runtimeType == other.runtimeType &&
+          left == other.left &&
+          right == other.right;
+
+  @override
+  int get hashCode => super.hashCode ^ left.hashCode ^ right.hashCode;
 }
-
-
-// TODO(netos23): add TypeForAll, TypeRec
-
-class TypeTuple extends StellaType {
-  final List<StellaType> types;
-
-  const TypeTuple({
-    required this.types,
-  });
-}
-
-class TypeRecord extends StellaType {
-  final Map<String, StellaType> types;
-
-  const TypeRecord({
-    required this.types,
-  });
-}
-
-class TypeVariant extends StellaType {
-  final Map<String, StellaType> types;
-
-  const TypeVariant({
-    required this.types,
-  });
-}
-
-class TypeList extends StellaType {
-  final StellaType type;
-
-  const TypeList({
-    required this.type,
-  });
-}
-
-class Unit extends StellaType {
-  const Unit();
-}
-
 
 class Func extends StellaType {
   final List<StellaType> args;
@@ -82,11 +73,182 @@ class Func extends StellaType {
   @override
   bool operator ==(Object other) =>
       identical(this, other) ||
-      other is Func &&
+      super == other &&
+          other is Func &&
           runtimeType == other.runtimeType &&
           args.equals(other.args) &&
           returnType == other.returnType;
 
   @override
-  int get hashCode => args.hashCode ^ returnType.hashCode;
+  int get hashCode => super.hashCode ^ args.hashCode ^ returnType.hashCode;
+}
+
+class TypeForAll extends StellaType {
+  final List<String> types;
+  final StellaType type;
+
+  const TypeForAll({
+    required this.types,
+    required this.type,
+  });
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      super == other &&
+          other is TypeForAll &&
+          runtimeType == other.runtimeType &&
+          types.equals(other.types) &&
+          type == other.type;
+
+  @override
+  int get hashCode => super.hashCode ^ types.hashCode ^ type.hashCode;
+}
+
+class TypeRec extends StellaType {
+  final String variable;
+  final StellaType type;
+
+  TypeRec({
+    required this.variable,
+    required this.type,
+  });
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      super == other &&
+          other is TypeRec &&
+          runtimeType == other.runtimeType &&
+          variable == other.variable &&
+          type == other.type;
+
+  @override
+  int get hashCode => super.hashCode ^ variable.hashCode ^ type.hashCode;
+}
+
+class TypeTuple extends StellaType {
+  final List<StellaType> types;
+
+  const TypeTuple({
+    required this.types,
+  });
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      super == other &&
+          other is TypeTuple &&
+          runtimeType == other.runtimeType &&
+          types.equals(other.types);
+
+  @override
+  int get hashCode => super.hashCode ^ types.hashCode;
+}
+
+class TypeRecord extends StellaType {
+  final Map<String, StellaType> types;
+
+  const TypeRecord({
+    required this.types,
+  });
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      super == other &&
+          other is TypeRecord &&
+          runtimeType == other.runtimeType &&
+          MapEquality().equals(types, other.types);
+
+  @override
+  int get hashCode => super.hashCode ^ types.hashCode;
+}
+
+class TypeVariant extends StellaType {
+  final Map<String, StellaType> types;
+
+  const TypeVariant({
+    required this.types,
+  });
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      super == other &&
+          other is TypeVariant &&
+          runtimeType == other.runtimeType &&
+          MapEquality().equals(types, other.types);
+
+  @override
+  int get hashCode => super.hashCode ^ types.hashCode;
+}
+
+class TypeList extends StellaType {
+  final StellaType type;
+
+  const TypeList({
+    required this.type,
+  });
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      super == other &&
+          other is TypeList &&
+          runtimeType == other.runtimeType &&
+          type == other.type;
+
+  @override
+  int get hashCode => super.hashCode ^ type.hashCode;
+}
+
+class Unit extends StellaType {
+  const Unit();
+}
+
+class Top extends StellaType {
+  const Top();
+}
+
+class Bottom extends StellaType {
+  const Bottom();
+}
+
+class Auto extends StellaType {
+  const Auto();
+}
+
+class TypeVar extends StellaType {
+  final String name;
+
+  const TypeVar(this.name);
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      super == other &&
+          other is TypeVar &&
+          runtimeType == other.runtimeType &&
+          name == other.name;
+
+  @override
+  int get hashCode => super.hashCode ^ name.hashCode;
+}
+
+class TypeParens extends StellaType {
+  final StellaType type;
+
+  const TypeParens(this.type);
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      super == other &&
+          other is TypeParens &&
+          runtimeType == other.runtimeType &&
+          type == other.type;
+
+  @override
+  int get hashCode => super.hashCode ^ type.hashCode;
 }
