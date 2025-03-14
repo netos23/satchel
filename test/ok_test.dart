42b6c11f430fbbba5a7fe3b08965487d3643b036
netos23 <nikmoroz2002@outlook.com>
2025-03-14 18:36:26 +0300

    fix(test): fix tests submodule

--- test/ok_test.dart
+++ test/ok_test.dart
@@ -4,16 +4,17 @@ import 'package:antlr4/antlr4.dart';
 import 'package:satchel/satchel.dart';
 import 'package:test/test.dart';
 
-void main(List<String> args) {
-  final basePath = args[0];
-  final dir = Directory(basePath);
+void main() {
+  group('Ok tests', ()  {
+    print(Directory.current);
+    const basePath = './test/stella-tests/ok';
+    final dir = Directory(basePath);
 
-  group('Ok tests', () async {
     setUpAll(() {
       ensureInitialized();
     });
 
-    await for (final file in dir.list()) {
+     for (final file in dir.listSync()) {
       test(file.path, () async {
         final input = await InputStream.fromPath(file.absolute.path);
         final report = buildStellaTypeReport(input);
