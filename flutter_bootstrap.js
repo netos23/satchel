4f7d66137d4f12fb752ead92f8a9c9468f6b0fd0
netos23 <netos23@users.noreply.github.com>
2025-04-15 11:35:52 +0000

    Deploying to gh-pages from @ netos23/satchel@4bb464d43c45dd94f2b61c68eeec9f51193b2189 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2255662026"
+    serviceWorkerVersion: "2967605596"
   }
 });
